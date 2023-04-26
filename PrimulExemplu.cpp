d4a020a7d46f8694f456775fbbc9a4af0a782718
zamfiroiu <alin.zamfiroiu@csie.ase.ro>
2023-04-26 17:07:50 +0300

    Add files via upload

---
+++ PrimulExemplu.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+//tipulReturnat numeFunctie([lista_parametri])
+
+void main() {
+
+	int variabila = 98;
+	float vReala = 67.56;
+	cout << variabila<<'\n'<<endl;
+	cout << vReala;
+
+	cout << "\nIntroduceti primul numar : ";
+	int a;
+	cin >> a;
+
+	cout << "\nIntroduceti al doilea numar : ";
+	int b;
+	cin >> b;
+
+	cout << "Suma celor doua numere este: " << a+b<<endl;
+	
+}
